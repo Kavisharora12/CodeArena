ce665d644e616f8ce8624bf3fe0992babae39213
aayushiuniyal <auniyal_be24@thapar.edu>
2026-09-04 15:49:03 +0530

    hello

--- codearena/src/components/Modals/ResetPassword.tsx
+++ codearena/src/components/Modals/ResetPassword.tsx
@@ -8,96 +8,96 @@ import { toast } from "react-toastify";
 type ResetPasswordProps = {};
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
-	const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
 
-	const [sendPasswordResetEmail, sending, error] =
-		useSendPasswordResetEmail(auth);
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
 
-	const handleReset = async (
-		e: React.FormEvent<HTMLFormElement>
-	) => {
-		e.preventDefault();
+  const handleReset = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
 
-		if (!email) {
-			toast.error("Please enter your email", {
-				position: "top-center",
-				autoClose: 3000,
-				theme: "dark",
-			});
-			return;
-		}
+    if (!email) {
+      toast.error("Please enter your email", {
+        position: "top-center",
+        autoClose: 3000,
+        theme: "dark",
+      });
+      return;
+    }
 
-		const success = await sendPasswordResetEmail(email);
+    const success = await sendPasswordResetEmail(email);
 
-		if (success) {
-			toast.success("Password reset email sent", {
-				position: "top-center",
-				autoClose: 3000,
-				theme: "dark",
-			});
+    if (success) {
+      toast.success("Password reset email sent", {
+        position: "top-center",
+        autoClose: 3000,
+        theme: "dark",
+      });
 
-			setEmail("");
-		} else {
-			toast.error("Password reset email could not be sent", {
-				position: "top-center",
-				autoClose: 3000,
-				theme: "dark",
-			});
-		}
-	};
+      setEmail("");
+    } else {
+      toast.error("Password reset email could not be sent", {
+        position: "top-center",
+        autoClose: 3000,
+        theme: "dark",
+      });
+    }
+  };
 
-	useEffect(() => {
-		if (error) {
-			toast.error(error.message, {
-				position: "top-center",
-				autoClose: 4000,
-				theme: "dark",
-			});
-		}
-	}, [error]);
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message, {
+        position: "top-center",
+        autoClose: 4000,
+        theme: "dark",
+      });
+    }
+  }, [error]);
 
-	return (
-		<form
-			className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8"
-			onSubmit={handleReset}
-		>
-			<h3 className="text-xl font-medium text-white">
-				Reset Password
-			</h3>
+  return (
+    <form
+      className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8"
+      onSubmit={handleReset}
+    >
+      <h3 className="text-xl font-medium text-white">
+        Reset Password
+      </h3>
 
-			<p className="text-sm text-white">
-				Forgotten your password? Enter your e-mail address below,
-				and we&apos;ll send you an e-mail allowing you to reset it.
-			</p>
+      <p className="text-sm text-white">
+        Forgotten your password? Enter your e-mail address below,
+        and we&apos;ll send you an e-mail allowing you to reset it.
+      </p>
 
-			<div>
-				<label
-					htmlFor="email"
-					className="text-sm font-medium block mb-2 text-gray-300"
-				>
-					Your email
-				</label>
+      <div>
+        <label
+          htmlFor="email"
+          className="text-sm font-medium block mb-2 text-gray-300"
+        >
+          Your email
+        </label>
 
-				<input
-					type="email"
-					name="email"
-					id="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-					placeholder="name@company.com"
-				/>
-			</div>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+          placeholder="name@company.com"
+        />
+      </div>
 
-			<button
-				type="submit"
-				disabled={sending}
-				className="w-full text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s disabled:opacity-50"
-			>
-				{sending ? "Sending..." : "Reset Password"}
-			</button>
-		</form>
-	);
+      <button
+        type="submit"
+        disabled={sending}
+        className="w-full text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s disabled:opacity-50"
+      >
+        {sending ? "Sending..." : "Reset Password"}
+      </button>
+    </form>
+  );
 };
 
 export default ResetPassword;
